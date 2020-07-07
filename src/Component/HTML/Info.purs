7588c34a9231c05a15c8613cf073e062c340f3f4
Nurlan Alkuatov <alkuatovnurlan@gmail.com>
2020-07-07 18:31:57 +0600

    Change the renderless info component

--- src/Component/HTML/Info.purs
+++ src/Component/HTML/Info.purs
@@ -2,40 +2,22 @@ module Joyland.Component.HTML.Info where
 
 import Joyland.HTML.Utils (css)
 
-import Prelude (($), show)
+import Prelude (($), show, (<>))
 import Halogen.HTML as HH
 
 info ∷ ∀ w i. { users ∷ Int, books ∷ Int} → HH.HTML w i
 info { users, books } =
   HH.nav
     [ css "level" ]
-    [ HH.div
-        [ css "level-item has-text-centered" ]
-        [ HH.div_
-            [ HH.p [ css "heading" ] [ HH.text "" ]
-            , HH.p [ css "title" ] [ HH.text ""]
-            ]
-        ]
+    [ HH.div [ css "level-left"] []
     , HH.div
-        [ css "level-item has-text-centered" ]
-        [ HH.div_
-            [ HH.p [ css "heading" ] [ HH.text "Пользователей" ]
-            , HH.p [ css "title" ] [ HH.text $ show users ]
-            ]
-        ]
-    , HH.div
-        [ css "level-item has-text-centered" ]
-        [ HH.div_
-            [ HH.p [ css "heading" ] [ HH.text "Книг" ]
-            , HH.p [ css "title" ] [ HH.text $ show books ]
-            ]
-        ]
-    , HH.div
-        [ css "level-item has-text-centered" ]
-        [ HH.div_
-            [ HH.p [ css "heading" ] [ HH.text "" ]
-            , HH.p [ css "title" ] [ HH.text ""]
-            ]
+        [ css "level-right" ]
+        [ HH.p
+            [ css "level-item has-text-info-dark" ]
+            [ HH.text $ show users <> " пользователей," ]
+        , HH.p
+            [ css "level-item has-text-info-dark" ]
+            [ HH.text $ show books <> " книг"]
         ]
     ]
 
