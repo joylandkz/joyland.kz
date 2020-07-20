4ccbc8d474bc69e90c9d3442bd8f97368aaef476
Nurlan Alkuatov <alkuatovnurlan@gmail.com>
2020-07-20 12:48:56 +0600

    Change Card component's markup

--- src/Component/HTML/Card.purs
+++ src/Component/HTML/Card.purs
@@ -1,17 +1,37 @@
-module Joyland.HTML.Card where
+module Joyland.HTML.Card
+  ( card
+  )
+  where
 
 import Joyland.HTML.Utils
 
-import Halogen as H
 import Halogen.HTML as HH
+
 
 card ∷ ∀ w i. HH.HTML w i
 card =
   HH.div
     [ css "card" ]
     [ HH.div [ css "card-content" ]
-        [ HH.div [ css "card-content" ]
-            [ HH.text "cards html works"]
+        [ media ]
+    ]
+
+media ∷ ∀ w i. HH.HTML w i
+media =
+  HH.div [ css "media" ]
+    [ HH.div [ css "media-left"]
+        [ HH.text "avatar" ]
+    , HH.div [ css "media-content" ]
+        [ HH.div [ css "content" ]
+            [ HH.p_
+                [ HH.strong_ [ HH.text "John Smith" ]
+                , HH.small_ [ HH.text "@johnsmith" ]
+                , HH.br_
+                , HH.text "some bio goes here"
+                ]
+            ]
         ]
+    , HH.div [ css "media-right" ]
+        [ HH.text "book image" ]
     ]
 
