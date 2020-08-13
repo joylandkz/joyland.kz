78444d2dc2405b7cb1acad891e11463df00ad5de
Nurlan Alkuatov <alkuatovnurlan@gmail.com>
2020-08-13 18:45:40 +0600

    Add a dummy instance for ManageAccounts AppM

--- src/AppM.purs
+++ src/AppM.purs
@@ -4,6 +4,9 @@ import Effect.Class (class MonadEffect)
 import Effect.Aff (Aff)
 import Effect.Aff.Class (class MonadAff)
 import Control.Monad
+import Joyland.Capability.Resource.Account (class ManageAccount)
+import Joyland.Data.Account (mockAccounts)
+
 
 -- | TODO: Change m to appropriate monad stack later
 newtype AppM a = AppM (Aff a)
@@ -15,4 +18,8 @@ derive newtype instance applicativeAppM ∷ Applicative AppM
 derive newtype instance monadAppM       ∷ Monad AppM
 derive newtype instance monadEffectAppM ∷ MonadEffect AppM
 derive newtype instance monadAffAppM    ∷ MonadAff AppM
+
+-- `fetchAccounts` should be replaced to a proper API call later
+instance appMManageAccount ∷ ManageAccount AppM where
+  fetchAccounts = pure mockAccounts
 
